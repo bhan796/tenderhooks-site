fa4230b029ee688d59c669e9aa51233b65415c03
benhannanosf <ben-hannan@outlook.com>
2026-03-15 19:27:13 +0000

    Simplify header by removing non-functional nav links

--- components/header.tsx
+++ components/header.tsx
@@ -1,31 +1,15 @@
 ﻿import Link from "next/link";
 import { Logo } from "./logo";
-import { MobileMenu } from "./mobile-menu";
 
 export const Header = () => {
   return (
     <div className="fixed z-50 pt-8 md:pt-14 top-0 left-0 w-full">
       <header className="flex items-center justify-between container">
         <Link href="/"><Logo className="w-[100px] md:w-[120px]" /></Link>
-        <nav className="flex max-lg:hidden absolute left-1/2 -translate-x-1/2 items-center justify-center gap-x-10">
-          {[
-            { label: "About", href: "/#about" },
-            { label: "Pricing", href: "/#pricing" },
-            { label: "Insights", href: "/#insights" },
-            { label: "Use Cases", href: "/#portfolio" },
-            { label: "Contact", href: "/#contact" },
-          ].map((item) => (
-            <Link className="uppercase inline-block font-mono text-foreground/60 hover:text-foreground/100 duration-150 transition-colors ease-out" href={item.href} key={item.label}>
-              {item.label}
-            </Link>
-          ))}
-        </nav>
-        <Link className="uppercase max-lg:hidden transition-colors ease-out duration-150 font-mono text-primary hover:text-primary/80" href="/onboarding">
+        <Link className="uppercase transition-colors ease-out duration-150 font-mono text-primary hover:text-primary/80" href="/onboarding">
           Start Trial
         </Link>
-        <MobileMenu />
       </header>
     </div>
   );
 };
-
